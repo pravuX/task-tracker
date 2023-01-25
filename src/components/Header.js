42ff318ba471d535244cff9b61601340b73868d7
Prabesh Subedi <prabesh.sb@gmail.com>
2023-01-26 01:50:30 +0545

    Add Task.js, Change indentation size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const lmao = () => {
+    console.log('from header')
+  }
+
   return (
-    <header>
-        <h1>{title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={lmao} />
     </header>
   )
 }
