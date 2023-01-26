6619606455b8a56d3b50d8b352f5587050f30149
Prabesh Subedi <prabesh.sb@gmail.com>
2023-01-26 18:22:04 +0545

    Add button toggle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
-  const 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -30,7 +30,7 @@ const App = () => {
   const addTask = (task) => {
     // create a random id for the new task
     const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = {id: id, ...task}
+    const newTask = { id: id, ...task }
     setTasks([...tasks, newTask])
   }
 
@@ -58,11 +58,15 @@ const App = () => {
   }
 
   return (
-    // using the ternary operator is display a message if
-    // there are no tasks left
+    // using the ternary operator to display
+    // a message if there are no tasks left
+
+    // using a shorthand ternary operator to
+    // show or hide the AddTask form
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+
       {tasks.length > 0 ?
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         :
