f0b6e6099367df6cd88fe1ba9ec8c791fcd3cfbe
Prabesh Subedi <prabesh.sb@gmail.com>
2023-01-26 15:36:52 +0545

    Add message when no tasks are left

--- src/App.js
+++ src/App.js
@@ -26,13 +26,23 @@ const App = () => {
 
   // Delete Task
   const deleteTask = (id) => {
-    console.log('delete', id)
+    // this is passing to setTasks the above tasks array but
+    // filtering the 'deleted' task from the tasks array
+    // each time when the setTasks is called with the filtered
+    // array, the Tasks and Task components get updated.
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   return (
+    // using the ternary operator is display a message if 
+    // there are no tasks left
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      { tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+        :
+        'All tasks completed!'
+      }
     </div>
   );
 }
