11e675804fa54baa1e92699ce8905f23f4cb9680
Prabesh Subedi <prabesh.sb@gmail.com>
2023-01-26 19:42:56 +0545

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -109,16 +112,38 @@ const App = () => {
 
     // using a shorthand ternary operator to
     // show or hide the AddTask form
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
+    <Router>
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
 
-      {tasks.length > 0 ?
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-        :
-        'All tasks completed!'
-      }
-    </div>
+        <Routes>
+          <Route
+            path='/'
+            exact
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder} />
+                ) : (
+                  'All tasks completed!'
+                )}
+              </>
+            }
+          />
+          <Route
+            path='/about'
+            element={
+              <About />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
